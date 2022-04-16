768cde48dc46b6255bac65fc6c0c4966a059bf9d
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-04-16 14:36:21 +0100

    Initial commit

---
+++ app/src/main/java/ipvc/cm/cm_aulas/MainActivity.kt
@@ -0,0 +1,11 @@
+package ipvc.cm.cm_aulas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
