f8a7b2baa02f7267705b870066ee3d0dcddb7260
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-04-16 20:01:37 +0100

    lab4-layouts

--- app/src/main/java/ipvc/cm/cm_aulas/MainActivity.kt
+++ app/src/main/java/ipvc/cm/cm_aulas/MainActivity.kt
@@ -2,10 +2,13 @@ package ipvc.cm.cm_aulas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello from layout", Toast.LENGTH_LONG).show()
     }
 }
