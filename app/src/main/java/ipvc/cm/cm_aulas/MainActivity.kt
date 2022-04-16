97412d3adcc5e02707ff79bad1ed8ab04f05eaf0
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-04-16 20:54:52 +0100

    lab5-ControlsComuns

--- app/src/main/java/ipvc/cm/cm_aulas/MainActivity.kt
+++ app/src/main/java/ipvc/cm/cm_aulas/MainActivity.kt
@@ -2,10 +2,26 @@ package ipvc.cm.cm_aulas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeName(view: View) {
+        val editText = findViewById<EditText>(R.id.et1)
+        val textView = findViewById<TextView>(R.id.tv1)
+
+        if(editText.text.toString() == ""){
+            Toast.makeText(this, "Please write your name", Toast.LENGTH_SHORT).show()
+        } else {
+            textView.setText(editText.text)
+            // textView.text = editText.text
+        }
     }
 }
